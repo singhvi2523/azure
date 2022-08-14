4dd03b645763458e38e84116e6b13e3f9a4643d0
Vikas Singh <singhvi252022@gmail.com>
2022-08-14 17:29:38 +0530

    first commit

---
+++ first/src/test/java/com/vikas/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
